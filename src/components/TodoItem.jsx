445559667859af2eb5fb39522fa5916977ab4fdc
unknown <roshankrm7@gmail.com>
2025-05-11 19:28:27 +0530

    Color & localStorage implemented

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -20,7 +20,7 @@ function TodoItem({todo}) {
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-        todo.isCompleted ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+        todo.isCompleted ? "bg-[#87fa89]" : "bg-[#fa9191]"
       }`}
     >
       <input
